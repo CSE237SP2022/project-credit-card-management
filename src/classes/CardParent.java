ded3ef3e9a710d5d37f87ad6d829e344856c003a
ayoo1234 <andrew.yoo@wustl.edu>
2022-03-28 12:51:40 -0500

    Done creating the member variables of the parent class

--- src/classes/CardParent.java
+++ src/classes/CardParent.java
@@ -1,5 +1,6 @@
 package classes;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;  
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -20,6 +21,7 @@ public class CardParent{
 		this.pin = pin;
 		this.cardNumber = generateCardNumber();
 		this.CVV = generateCVV();
+		this.expDate = generateExpDate();
 		
 		System.out.println(java.time.LocalDate.now());
 		
@@ -50,13 +52,25 @@ public class CardParent{
 		Random rand = new Random();
 		ArrayList<Integer> cvvNumberList  = new ArrayList<Integer>();
 		
-		int randomInt = rand.nextInt();
+		for(int i = 0; i < 3; i++)
+		{
+			int randomInt = rand.nextInt(10);
+			cvvNumberList.add(randomInt);
+		}
 		
-		for(int i = 0; i < 15; i++)
+		Integer[] cvvNumberArray = cvvNumberList.toArray(new Integer[0]);
 		
-		return 0;
+		return cvvNumberArray;
 	}
 	
+	public String generateExpDate()
+	{
+		LocalDate today = java.time.LocalDate.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy MM dd");
+		String formattedString = today.format(formatter);
+
+		return formattedString;
+	}
 	
 }
 
