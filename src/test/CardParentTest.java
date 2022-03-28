83ea3929c6d96ef4c20d1db90fee883790c39bbc
ayoo1234 <andrew.yoo@wustl.edu>
2022-03-28 00:07:56 -0500

    Tests for CardParent class complete

--- src/test/CardParentTest.java
+++ src/test/CardParentTest.java
@@ -69,25 +69,96 @@ class CardParentTest {
 		if (intNameSize == 2)
 		{
 			String firstName = nameList[0];
-			String LastName = nameList[1];
+			String lastName = nameList[1];
 			
 			boolean isFirstNameCapitalized = false;
 			boolean isLastNameCapitalized = false;
 			
 			if (Character.isUpperCase(firstName.charAt(0)))
 			{
-				
+				isFirstNameCapitalized = true;
 			}
 			
-
+			if (Character.isUpperCase(lastName.charAt(0)))
+			{
+				isLastNameCapitalized = true;
+			}
+			
+			assertEquals(true, isFirstNameCapitalized);
+			assertEquals(true, isLastNameCapitalized);
 		}
 		
+		else if (intNameSize == 3)
+		{
+			String firstName = nameList[0];
+			String middleName = nameList[1];
+			String lastName = nameList[2];
+			
+			boolean isFirstNameCapitalized = false;
+			boolean isMiddleNameCapitalized = false;
+			boolean isLastNameCapitalized = false;
+			
+			if (Character.isUpperCase(firstName.charAt(0)))
+			{
+				isFirstNameCapitalized = true;
+			}
+			
+			if (Character.isUpperCase(middleName.charAt(0)))
+			{
+				isMiddleNameCapitalized = true;
+			}
+			
+			if (Character.isUpperCase(lastName.charAt(0)))
+			{
+				isLastNameCapitalized = true;
+			}
+			
+			assertEquals(true, isFirstNameCapitalized);
+			assertEquals(true, isMiddleNameCapitalized);
+			assertEquals(true, isLastNameCapitalized);
+		}
 	}
 	
 	@Test
 	void testHasValidExpirationDate()
 	{
+		//2022-03-27
 		
+		String date = this.card.getExpDate();
+		
+		String dateList[] = date.split("-");
+		
+		String year = dateList[0];
+		String month = dateList[1];
+		String day = dateList[2];
+		
+		int yearDigits = year.length();
+		int monthDigits = month.length();
+		int dayDigits = day.length();
+		
+		int yearInt = Integer.parseInt(year);
+		int monthInt = Integer.parseInt(month);
+		int dayInt = Integer.parseInt(day);
+		
+		boolean isMonthValid = false;
+		boolean isDayValid = false;
+		
+		if (monthInt>0 && monthInt<13)
+		{
+			isMonthValid = true;
+		}
+		
+		if (dayInt>0 && dayInt<32)
+		{
+			isDayValid = true;
+		}
+		
+		assertEquals(true, isMonthValid);
+		assertEquals(true, isDayValid);
+		
+		assertEquals(4, yearDigits);
+		assertEquals(2, monthDigits);
+		assertEquals(2, dayDigits);
 	}
 
 }
