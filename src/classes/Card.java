f45df21f5eabb1c166adb915a87ab4634618a520
DuBose Tuller <d.tuller@wustl.edu>
2022-03-28 13:23:23 -0500

    Added Card mvs and getters

--- src/classes/Card.java
+++ src/classes/Card.java
@@ -1,10 +1,40 @@
 package classes;
 
+import java.util.ArrayList;
+
 public class Card {
+	
+	private String cardNumber;
+	private double balance;
+	private ArrayList<Transaction> transactionHistory;
+	
+	
+	public Card() {
+		this.cardNumber = "";
+		this.balance = 0;
+		this.transactionHistory = new ArrayList<Transaction>();
+	}
 
 	public String getCardNumber() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.cardNumber;
 	}
+	
+	public double getBalance() {
+		return this.balance;
+	}
+	
+//	public boolean spend(Transaction transaction) {
+//		double price = transaction.getPrice();
+//		
+//		if (price <= this.balance) {
+//			this.balance -= price;
+//			this.transactionHistory.add(transaction);
+//			
+//			return true;
+//		} else {
+//			return false;
+//		}
+//	}
+	
 
 }
---
+++ src/test/testCard.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testCard {
+
+	@Test
+	void testSpend() {
+		fail("Not yet implemented");
+	}
+
+}
