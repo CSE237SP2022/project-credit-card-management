196a2dbd397c2ab7cbe79b52d50091027d50e061
ayoo1234 <andrew.yoo@wustl.edu>
2022-03-27 23:09:51 -0500

    Added to test file of card parent

--- src/classes/CardParent.java
+++ src/classes/CardParent.java
@@ -1,5 +1,23 @@
 package classes;
 
-public class CardParent {
+import java.time.LocalDateTime;  
+import java.time.format.DateTimeFormatter;  
 
+public class CardParent{
+	
+	private String name;
+	private int cardNumber;
+	private int CVV;
+	private String expDate;
+	private int pin;
+	
+	
+	public CardParent(String name, int pin)
+	{
+		//Auto-generate card number, CVV, and expiration date
+		this.name = name;
+		this.pin = pin;
+		System.out.println(java.time.LocalDate.now());
+	}	
 }
+
--- src/test/CardParentTest.java
+++ src/test/CardParentTest.java
@@ -1,14 +1,93 @@
 package test;
 
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class CardParentTest {
+import classes.CardParent;
 
+
+
+class CardParentTest {
+	
+	private CardParent card;
+
+	@BeforeEach
+	void setup() {
+		String name = "George Washington";
+		int pin = 1234;
+		card = new CardParent(name, pin);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testHasValidCardNumber() 
+	{
+		//Must be certain length: 16 digits
+		//Card Cannot start with the number 0
+		
+		int cardNumber = this.card.getCardNumber();
+		String cardNumberString = String.valueOf(cardNumber);
+		int cardNumberDigits = cardNumberString.length();
+		
+		char firstDigit = cardNumberString.charAt(0);
+		
+		assertEquals(16, cardNumberDigits);
+		assertNotEquals("0", firstDigit);
+	}
+	
+	@Test
+	void testHasValidCVVCode()
+	{
+		//Must be certain length: 3 digits
+		
+		int cvvCode = this.card.getCardCVV();
+		String cardCVVString = String.valueOf(cvvCode);
+		int cvvCodeDigits = cardCVVString.length();
+		
+		assertEquals(3, cvvCodeDigits);
+	}
+	
+	@Test
+	void testHasValidPinNumber()
+	{
+		int pin = this.card.getCardPin();
+		String cardPinString = String.valueOf(pin);
+		int pinDigits = cardPinString.length();
+		
+		assertEquals(3, pinDigits);
+	}
+	
+	@Test
+	void testHasValidName()
+	{
+		String name = this.card.getName();
+		
+		String nameList[] = name.split(" ");
+		int intNameSize = nameList.length;
+		
+		if (intNameSize == 2)
+		{
+			String firstName = nameList[0];
+			String LastName = nameList[1];
+			
+			boolean isFirstNameCapitalized = false;
+			boolean isLastNameCapitalized = false;
+			
+			if (Character.isUpperCase(firstName.charAt(0)))
+			{
+				
+			}
+			
+
+		}
+		
+	}
+	
+	@Test
+	void testHasValidExpirationDate()
+	{
+		
 	}
 
 }
